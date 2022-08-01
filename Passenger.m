3240f7383b444d5dfce38cf4156d14c74169b618
xiaohan <102499641+dashXiao@users.noreply.github.com>
2022-08-01 15:38:53 +0800

    Update Passenger.m

--- Passenger.m
+++ Passenger.m
@@ -24,13 +24,28 @@
 @synthesize history;
 @synthesize future;
 
+//订票：将订单添入未出行订单数组
 - (void) makeOrders:(Orders *)od{
     [future addObject:od];
 }
 
+//检票：将订单移出未出行订单数组，添入历史订单数组
 - (void) check:(Orders*)od{
     [future removeObject:od];
     [history addObject:od];
 }
 
+//查看未出行订单
+- (void) printFuture:(NSMutableArray *)array{
+    NSLog(@"未出行订单");
+    for(id od in array)
+        [od print];
+}
+//查看历史订单
+- (void) printHistory:(NSMutableArray*)array{
+    NSLog(@"历史订单");
+    for(id od in array)
+        [od print];
+}
+          
 @end
